eee4aca84be94607cae366768e3424ef2bf68347
chebrolus3 <159662285+chebrolus3@users.noreply.github.com>
2024-04-01 22:09:17 -0400

    Add files via upload

---
+++ dev.tf
@@ -0,0 +1,14 @@
+module "module_dev" {
+    source = "./modules"
+    resource_group_name              = "dev-resource-group"
+    location                         = "East US"
+    vnet                             = "dev-vnet1"
+    vm_name                          = "dev-vm"
+    vm_size                          = "Standard_DS1_v2"
+    key_vault_name                   = "dev-keyy-vault"
+    nicname                          = "dev12NIC"
+    subnet                           = "dev-subnet"
+    storage_account_name             = "dev12storageaccount"
+    storage_account_tier             = "Standard"
+    storage_account_replication_type = "LRS"
+}
