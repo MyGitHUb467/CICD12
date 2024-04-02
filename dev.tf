c144f1598a14e59ae0f35d4773b7b2301705ddfb
chebrolus3 <159662285+chebrolus3@users.noreply.github.com>
2024-04-01 22:09:43 -0400

    Update dev.tf

--- dev.tf
+++ dev.tf
@@ -8,7 +8,7 @@ module "module_dev" {
     key_vault_name                   = "dev-keyy-vault"
     nicname                          = "dev12NIC"
     subnet                           = "dev-subnet"
-    storage_account_name             = "dev12storageaccount"
+    storage_account_name             = "storage93987"
     storage_account_tier             = "Standard"
     storage_account_replication_type = "LRS"
 }
