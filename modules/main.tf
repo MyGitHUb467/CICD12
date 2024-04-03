2dae365bc97b50837c950a90949fa53254830ebe
MyDevOps13 <159662285+MyGitHUb467@users.noreply.github.com>
2024-04-03 14:58:28 -0400

    Update main.tf

--- modules/main.tf
+++ modules/main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
 
 data "azurerm_client_config" "current" {}
 
-# Resource Group
+
 resource "azurerm_resource_group" "R1" {
   name     = var.resource_group_name
   location = var.location
@@ -35,7 +35,7 @@ resource "azurerm_subnet" "example" {
   name                 = var.subnet
   resource_group_name  = azurerm_resource_group.R1.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
-  address_prefixes     = ["10.20.1.0/24"] # Adjust this according to your network requirements
+  address_prefixes     = ["10.20.1.0/24"] 
 }
 
 resource "azurerm_network_interface" "nic1" {
